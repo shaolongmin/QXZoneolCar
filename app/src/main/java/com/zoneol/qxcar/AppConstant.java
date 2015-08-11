a6b01c29b37a769f463c11aa6cb3654aa25c65ca
shaolongmin <shaolongMin@gmail.com>
2015-08-11 19:23:56 +0800

    test first commit

--- app/src/main/java/com/zoneol/qxcar/AppConstant.java
+++ app/src/main/java/com/zoneol/qxcar/AppConstant.java
@@ -4,6 +4,7 @@ package com.zoneol.qxcar;
  * Created by shaolong on 2015/8/11.
  */
 public class AppConstant {
+    public static final String myName = "sfdsf" ;
     public static final int MAIN_TAB_INDEX = 0 ;
     public static final int MAIN_TAB_FIRST = 1 ;
     public static final int MAIN_TAB_SECOND = 2 ;
